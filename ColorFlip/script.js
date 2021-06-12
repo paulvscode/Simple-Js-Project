8f54ac2caee9191fa69d7fc426d1e75a6e2c2f3c
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-06-12 22:35:49 +0200

    added hex color

--- ColorFlip/script.js
+++ ColorFlip/script.js
@@ -1,4 +1,5 @@
 const btnColor = document.querySelector(".btn");
+const hexDiv = document.querySelector(".hex-div");
 const bg = document.getElementById("background");
 
 btnColor.addEventListener("click", randomHex);
@@ -12,6 +13,14 @@ function randomHex() {
   replaceBg(arrayRand.join(""));
 }
 
+function invertHex(hex) {
+  return (Number(`0x1${hex}`) ^ 0xffffff).toString(16).substr(1).toUpperCase();
+}
+
 function replaceBg(hexNumber) {
   bg.style.backgroundColor = "#" + hexNumber;
+  let newHexNumber = invertHex(hexNumber);
+  console.log(newHexNumber);
+  hexDiv.style.color = `#${newHexNumber}`;
+  hexDiv.innerHTML = "#" + hexNumber;
 }
