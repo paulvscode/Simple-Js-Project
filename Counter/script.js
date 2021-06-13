6d8d6c6591c24bfed455e5df0a3412ae01a6fba3
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-06-13 07:49:43 +0200

    added reset button

--- Counter/script.js
+++ Counter/script.js
@@ -1,12 +1,14 @@
 const counterText = document.querySelector(".counter-text");
 const minusButton = document.querySelector(".minus");
 const plusButton = document.querySelector(".plus");
+const resetButton = document.querySelector(".reset-counter");
 
 let valueCounter = 0;
 counterText.innerHTML = valueCounter;
 
 minusButton.addEventListener("click", subtractFromCounter);
 plusButton.addEventListener("click", addToCounter);
+resetButton.addEventListener("click", resetCounter);
 
 function subtractFromCounter() {
   if (valueCounter <= 0) {
@@ -18,5 +20,10 @@ function subtractFromCounter() {
 
 function addToCounter() {
   valueCounter++;
+  counterText.innerHTML = valueCounter;
+}
+
+function resetCounter() {
+  valueCounter = 0;
   counterText.innerHTML = valueCounter;
 }
